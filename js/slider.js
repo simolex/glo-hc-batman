1a22125f2d37541cdbcd432bf3f39e60e2096c5e
simolex <simolex@yandex.ru>
2022-02-13 12:00:22 +0300

    Мелкие правки

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,14 @@
 const slider = () => {
   const hotelSlider = new Swiper(".main__slider", {
-    // Optional parameters
-    //direction: "vertical",
     loop: true,
-    //effect: "flip",
+    slidesPerView: 2,
+    spaceBetween: 28,
     keyboard: {
       enabled: true,
       onlyInViewport: true,
     },
-    slidesPerView: 2,
-    spaceBetween: 28,
-    // Navigation arrows
     navigation: {
       nextEl: ".main__button-next",
-      //prevEl: ".hotel-slider__button--prev",
     },
   });
 };
